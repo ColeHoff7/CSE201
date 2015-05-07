1d8fe1de6b0eb82c6516e267a0348bb4fe978a39
Morgan Jacobson <jacobsmw@miamioh.edu>
2015-05-07 02:00:07 -0400

    Working on seed

--- AppYours/db/seeds.rb
+++ AppYours/db/seeds.rb
@@ -6,25 +6,204 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-test_app = App.create(name: 'test',appl_plat: false, andr_plat: true, wind_plat: false,
-developer: 'Adam Siens', andr_vers: '1.2.3', appl_vers: '1.2.3', wind_vers: '1.2.3',
-andr_store: 'Andr Store',wind_store:'Wind Store', appl_store:'Appl Store',
-andr_price: 0,appl_price: 0, wind_price: 0,
-# picture1: 'P1',picture2:'P2',picture3: 'P3',video: 'video',
- description:'This is the text, that is going in the test.', catagory: 'Games')
+App.create(
+name: "The Marauder's Map",
+description:'Tired of getting lost on the way to class? Forget that the grand staircase vanishes between 10:37 and 10:52? Ever open a door to find a brick wall in front of you? Well worry no more! With this app, all of hogwarts secrets are revealed to you!', 
+appl_plat: true, appl_price: 2.00, appl_vers: '1.2', appl_store: 'https://itunes.apple.com', 
+andr_plat: true, andr_price: 2.50, andr_vers: '1.3', andr_store: 'https://play.google.com/store', 
+wind_plat: false, wind_price: 0, wind_vers: '1.2.3', wind_store:'http://www.windowsphone.com/en-us/store', 
+developer: "'Warts Tech",
+catagory: 'Utility',
+logo: 'www.stuff.com', pic1: 'www.stuff.com',
+pic2: 'www.stuff.com', pic3: 'www.stuff.com',
+pic4: 'www.stuff.com', pic5: 'www.stuf.com')
 
-User.create!(name:  "Example User",
-             email: "example@railstutorial.org",
-            password:              "foobar",
-           password_confirmation: "foobar",
-          admin: true)
+App.create(
+name: 'Giant Squid Watch',
+description:"Keep tabs on the school's freindliest pet with this handy dandy app", 
+appl_plat: false, appl_price: 0, appl_vers: '1.2.3', appl_store: 'https://itunes.apple.com', 
+andr_plat: true, andr_price: 0, andr_vers: '1.2.3', andr_store: 'https://play.google.com/store', 
+wind_plat: false, wind_price: 0, wind_vers: '1.2.3', wind_store:'http://www.windowsphone.com/en-us/store', 
+developer: 'Adam Siens',
+catagory: 'Games',
+logo: 'www.stuff.com', pic1: 'www.stuff.com',
+pic2: 'www.stuff.com', pic3: 'www.stuff.com',
+pic4: 'www.stuff.com', pic5: 'www.stuf.com')
 
-99.times do |n|
-  name  = Faker::Name.name
-  email = "example-#{n+1}@railstutorial.org"
-  password = "password"
-  User.create!(name:  name,
-               email: email,
-               password:              password,
-               password_confirmation: password)
-end
+App.create(
+name: 'Fantasy Quidditch',
+description:'This is the text, that is going in the test.', 
+appl_plat: false, appl_price: 0, appl_vers: '1.2.3', appl_store: 'https://itunes.apple.com', 
+andr_plat: true, andr_price: 0, andr_vers: '1.2.3', andr_store: 'https://play.google.com/store', 
+wind_plat: false, wind_price: 0, wind_vers: '1.2.3', wind_store:'http://www.windowsphone.com/en-us/store', 
+developer: 'Adam Siens',
+catagory: 'Games',
+logo: 'www.stuff.com', pic1: 'www.stuff.com',
+pic2: 'www.stuff.com', pic3: 'www.stuff.com',
+pic4: 'www.stuff.com', pic5: 'www.stuf.com')
+
+App.create(
+name: 'Students Out of Bed',
+description:'This is the text, that is going in the test.', 
+appl_plat: false, appl_price: 0, appl_vers: '1.2.3', appl_store: 'https://itunes.apple.com', 
+andr_plat: true, andr_price: 0, andr_vers: '1.2.3', andr_store: 'https://play.google.com/store', 
+wind_plat: false, wind_price: 0, wind_vers: '1.2.3', wind_store:'http://www.windowsphone.com/en-us/store', 
+developer: 'Adam Siens',
+catagory: 'Games',
+logo: 'www.stuff.com', pic1: 'www.stuff.com',
+pic2: 'www.stuff.com', pic3: 'www.stuff.com',
+pic4: 'www.stuff.com', pic5: 'www.stuf.com')
+
+App.create(
+name: 'Ghost Tracker',
+description:'This is the text, that is going in the test.', 
+appl_plat: false, appl_price: 0, appl_vers: '1.2.3', appl_store: 'https://itunes.apple.com', 
+andr_plat: true, andr_price: 0, andr_vers: '1.2.3', andr_store: 'https://play.google.com/store', 
+wind_plat: false, wind_price: 0, wind_vers: '1.2.3', wind_store:'http://www.windowsphone.com/en-us/store', 
+developer: 'Adam Siens',
+catagory: 'Games',
+logo: 'www.stuff.com', pic1: 'www.stuff.com',
+pic2: 'www.stuff.com', pic3: 'www.stuff.com',
+pic4: 'www.stuff.com', pic5: 'www.stuf.com')
+
+App.create(
+name: 'IQA Updates',
+description:'This is the text, that is going in the test.', 
+appl_plat: false, appl_price: 0, appl_vers: '1.2.3', appl_store: 'https://itunes.apple.com', 
+andr_plat: true, andr_price: 0, andr_vers: '1.2.3', andr_store: 'https://play.google.com/store', 
+wind_plat: false, wind_price: 0, wind_vers: '1.2.3', wind_store:'http://www.windowsphone.com/en-us/store', 
+developer: 'Adam Siens',
+catagory: 'Games',
+logo: 'www.stuff.com', pic1: 'www.stuff.com',
+pic2: 'www.stuff.com', pic3: 'www.stuff.com',
+pic4: 'www.stuff.com', pic5: 'www.stuf.com')
+
+App.create(
+name: 'Hogsmead Deals',
+description:'This is the text, that is going in the test.', 
+appl_plat: false, appl_price: 0, appl_vers: '1.2.3', appl_store: 'https://itunes.apple.com', 
+andr_plat: true, andr_price: 0, andr_vers: '1.2.3', andr_store: 'https://play.google.com/store', 
+wind_plat: false, wind_price: 0, wind_vers: '1.2.3', wind_store:'http://www.windowsphone.com/en-us/store', 
+developer: 'Adam Siens',
+catagory: 'Games',
+logo: 'www.stuff.com', pic1: 'www.stuff.com',
+pic2: 'www.stuff.com', pic3: 'www.stuff.com',
+pic4: 'www.stuff.com', pic5: 'www.stuf.com')
+
+App.create(
+name: 'Spell Reminder',
+description:'This is the text, that is going in the test.', 
+appl_plat: false, appl_price: 0, appl_vers: '1.2.3', appl_store: 'https://itunes.apple.com', 
+andr_plat: true, andr_price: 0, andr_vers: '1.2.3', andr_store: 'https://play.google.com/store', 
+wind_plat: false, wind_price: 0, wind_vers: '1.2.3', wind_store:'http://www.windowsphone.com/en-us/store', 
+developer: 'Adam Siens',
+catagory: 'Games',
+logo: 'www.stuff.com', pic1: 'www.stuff.com',
+pic2: 'www.stuff.com', pic3: 'www.stuff.com',
+pic4: 'www.stuff.com', pic5: 'www.stuf.com')
+
+App.create(
+name: 'Magical Animal Identifier',
+description:'This is the text, that is going in the test.', 
+appl_plat: false, appl_price: 0, appl_vers: '1.2.3', appl_store: 'https://itunes.apple.com', 
+andr_plat: true, andr_price: 0, andr_vers: '1.2.3', andr_store: 'https://play.google.com/store', 
+wind_plat: false, wind_price: 0, wind_vers: '1.2.3', wind_store:'http://www.windowsphone.com/en-us/store', 
+developer: 'Adam Siens',
+catagory: 'Games',
+logo: 'www.stuff.com', pic1: 'www.stuff.com',
+pic2: 'www.stuff.com', pic3: 'www.stuff.com',
+pic4: 'www.stuff.com', pic5: 'www.stuf.com')
+
+App.create(
+name: 'test',
+description:'This is the text, that is going in the test.', 
+appl_plat: false, appl_price: 0, appl_vers: '1.2.3', appl_store: 'https://itunes.apple.com', 
+andr_plat: true, andr_price: 0, andr_vers: '1.2.3', andr_store: 'https://play.google.com/store', 
+wind_plat: false, wind_price: 0, wind_vers: '1.2.3', wind_store:'http://www.windowsphone.com/en-us/store', 
+developer: 'Adam Siens',
+catagory: 'Games',
+logo: 'www.stuff.com', pic1: 'www.stuff.com',
+pic2: 'www.stuff.com', pic3: 'www.stuff.com',
+pic4: 'www.stuff.com', pic5: 'www.stuf.com')
+
+User.create!( name:  "Albus Dumbledore",
+              email: "adumbledor@hogwarts.edu",
+              password:              "password",
+              password_confirmation: "password",
+              admin: true)
+
+User.create!( name:  "Harry Potter",
+              email: "hpotter@hogwarts.edu",
+              password:              "password",
+              password_confirmation: "password",
+              admin: false)
+
+User.create!( name:  "Hermione Granger",
+              email: "hgranger@hogwarts.edu",
+              password:              "password",
+              password_confirmation: "password",
+              admin: false)
+
+User.create!( name:  "Ron Weasley",
+              email: "rweasly@hogwarts.edu",
+              password:              "password",
+              password_confirmation: "password",
+              admin: false)
+
+User.create!( name:  "Ginny Weasley",
+              email: "gweasley2@hogwarts.edu",
+              password:              "password",
+              password_confirmation: "password",
+              admin: false)
+
+User.create!( name:  "Fred Weasley",
+              email: "fweasley@hogwarts.edu",
+              password:              "password",
+              password_confirmation: "password",
+              admin: false)
+
+User.create!( name:  "George Weasley",
+              email: "gweasley@hogwarts.edu",
+              password:              "password",
+              password_confirmation: "password",
+              admin: false)
+
+User.create!( name:  "Precy Weasley",
+              email: "weasleyp@ministry.gov",
+              password:              "password",
+              password_confirmation: "password",
+              admin: false)
+
+User.create!( name:  "Charlie Weasley",
+              email: "charlesweasley@gringotts.org",
+              password:              "password",
+              password_confirmation: "password",
+              admin: false)
+
+User.create!( name:  "Bill Weasley",
+              email: "billweasley@wiznet.com",
+              password:              "password",
+              password_confirmation: "password",
+              admin: false)
+
+User.create!( name:  "Arthur Weasley",
+              email: "weaslya@ministry.gov",
+              password:              "password",
+              password_confirmation: "password",
+              admin: false)
+
+User.create!( name:  "Molly Weasley",
+              email: "mommamolly@wiznet.com",
+              password:              "password",
+              password_confirmation: "password",
+              admin: false)
+
+# 99.times do |n|
+#   name  = Faker::Name.name
+#   email = "example-#{n+1}@railstutorial.org"
+#   password = "password"
+#   User.create!(name:  name,
+#                email: email,
+#                password:              password,
+#                password_confirmation: password)
+# end
